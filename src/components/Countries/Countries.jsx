11d9f55ab76909d542d505af558dcf51705f4e8e
asheq1 <bsmrstu18fmb025@gmail.com>
2024-08-31 23:29:51 +0600

    lifting up

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,7 +5,8 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
-    
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
     useEffect(()=>{
         fetch(`https://restcountries.com/v3.1/all`)
         .then(res => res.json())
@@ -13,12 +14,42 @@ const Countries = () => {
 
     }, [])
 
+
+    const handleVisitedCountry = country => {
+        console.log('Add this to your visited country');
+        // visitedCountries.push(country)
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries)   
+
+    }
+
+
     return (
-        <div className="country-container">
+        <div>
             <h2>Countries {countries.length}</h2>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
+            <div>
+                <h4>Visited countries: {visitedCountries.length}</h4>
+                <ul className="countries">
+                    {
+                        visitedCountries.map(country => <li 
+                            key={country.cca3}
+                           style={{color: black}}
+                            >
+                            {country.name}
+                        </li>)
+                    }
+                </ul>
+            </div>
+
+            <div className="country-container">
+                {
+                    countries.map(country => <Country 
+                        key={country.cca3} 
+                        country={country}
+                        handleVisitedCountry={handleVisitedCountry}
+                        ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Countries/Country.jsx
+++ src/components/Countries/Country.jsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
-    console.log(country)
-    const {name, flags, area, population} = country;
+const Country = ({country, handleVisitedCountry}) => {
+    // console.log(country)
+    const {name, flags, area, population, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        // setVisited(true)
+        // toggle
+        setVisited(!visited)
+    }
+
 
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>        
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color : visited ? 'Purple' : 'black'}}>Name: {name?.common}</h3>        
             <img src={flags.png} alt="" />
             <p>Area: {area}</p>
             <p>Population: {population}</p>
+            <p>Country code: <small>{cca3}</small></p>
+            <button onClick={handleVisitedCountry}>Mark Visited</button>
+            <button onClick={()=> handleVisited(country)}>{visited ? 'Visited' : 'Going'}</button>
+            {/* {visited && 'I have visited this country.'} */}
+            {visited ? 'I have visited this country.': 'I went to go'}
+
         </div>
     );
 };
